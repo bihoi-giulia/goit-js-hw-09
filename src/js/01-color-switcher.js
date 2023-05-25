8feedaad5e799d4ead49773c3a977146d3e64b31
bihoi-giulia <bihoigiulia@gmail.com>
2023-05-25 21:08:45 +0300

    ex 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+    startBtn.disabled = true;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
